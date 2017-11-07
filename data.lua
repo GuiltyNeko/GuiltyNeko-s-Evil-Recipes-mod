2c80565292bf052b081155186ae0a8bde43781d2
GuiltyNeko <rulerofdabacon@gmail.com>
2017-11-07 00:39:53 -0800

    0.0.2
    Added Evil stone furnace recipe, normal & expensive

---
+++ data.lua
@@ -0,0 +1,1 @@
+require ("prototypes.GNER-stone-furnace")
---
+++ prototypes/GNER-stone-furnace.lua
@@ -0,0 +1,14 @@
+data.raw.recipe["stone-furnace"].expensive = {
+    ingredients = {
+        {"stone-brick", 10}
+    },
+    result = 'stone-furnace',
+}
+
+local recipe = data.raw.recipe["stone-furnace"]
+recipe.normal = {
+    ingredients = {
+    	{"stone", 10}
+	},
+    result = 'stone-furnace'
+}
